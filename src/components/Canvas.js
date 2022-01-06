20c840accbf25e4dd581c4ac22f555c343de8de4
Jack White <jfw793@gmail.com>
2022-01-06 15:24:44 +1100

    Fixed bug with incorrect stepping

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -9,27 +9,42 @@ const Canvas = (props) => {
         ctx.fillRect(x, y, 1, 1) //x, y, height, width
     }
 
-
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
 
         canvas.width = props.width
         canvas.height = props.height
-        
+
+        let colorArray = []
+
+        // Generate all 8-step values from rgb(0,0,0) to rgb(256,256,256) into array
+        for (let i = 1; i < 33; i++) {
+            const red = i * 8
+            for (let i = 1; i < 33; i++) {
+                const green = i * 8
+                for (let i = 1; i < 33; i++) {
+                    const blue = i * 8
+                    colorArray.push({
+                        red: red,
+                        green: green,
+                        blue: blue
+                    })
+                }
+            }   
+        }
+
+        // Draw all values from array of colours
         for (let i = 0; i < 32768; i++) {
             const x = i & 255;
             const y = i / 256;
-
-            const r = (i << 3) & 0xff;
-            const g = (i >> 2) & 0xff;
-            const b = (i >> 7) & 0xff;
-            draw(context, `rgb(${r}, ${g}, ${b})`, x, y)
+            draw(context, `rgb(${colorArray[i].red}, ${colorArray[i].green}, ${colorArray[i].blue})`, x, y)
         }
-    }, [draw])
+    })
+
 
     return (
-        <canvas ref={canvasRef}/>
+        <canvas ref={canvasRef} />
     )
 }
 
